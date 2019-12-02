3774a545b0115f448da242b552371c31d64f192e
HamamEL <hamam.alsaiity@gmail.com>
2019-12-02 00:58:44 -0800

    unfinished

--- assessment.js
+++ assessment.js
@@ -43,27 +43,28 @@ function daBears() {
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 =  ["papaBear", "mamaBear"];
+
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear",  "mamaBear",  "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = [ "daBears", "papaBear", "mamaBear", "babyBear"];
 
 
 // *************
@@ -82,6 +83,16 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // "charger" once, and invoke it twice on "mustang".
 
 // CODE HERE...
+
+  var Vehicle = function() {
+  this.gasRemaining = 100;
+
+}
+Vehicle.prototype.drive = function() {
+  this.gasRemaining  = this.gasRemaining - 25;
+}
+var charger = new Vehicle();
+var mustang = new Vehicle();
 
 
 
@@ -108,6 +119,15 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 
 // CODE HERE...
+string.prototype.grammarPolice = function(){
+    var array = this.split()
+    for ( i = 0; i < array.length; i++){
+        array[i]= array[i]charAt(0).toUpperCase()+ arr[i].slice(1).toLowerCase();
+
+    }
+ return arr.join(' ');
+}
+
 
 
 
@@ -126,6 +146,17 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // In all other cases, return "Different values"
 
 // CODE HERE...
+
+function valuetype(arg1,arg2){
+if(arg1 === arg2) {
+return "Exactly the same";
+}
+    else if (arg1 == arg2){
+ return "Same value, different types";
+}
+        return "Different values";
+}
+
 
 // *************
 // * PROBLEM 5 *
